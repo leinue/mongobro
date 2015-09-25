cb4490cd4c400e15ec800e8b899d42f3d7d86f1f
ivy <597055914@qq.com>
2015-09-26 00:41:17 +0800

    update terminal

--- js/controller.js
+++ js/controller.js
@@ -17,31 +17,88 @@ $(function() {
 
 	//终端窗口状态
 	var terminalStatus = {
-		isMaxified : false, //是否最大化,默认为假
-		isMinified : false, //是否最小化,默认为假
-		isClosed : true, //是否被关闭,默认为真
-		isShow : false //是否被显示,默认为假
+		isMaxified : false, //终端窗口是否最大化,默认为假
+		isMinified : false, //终端窗口是否最小化,默认为假
+		isClosed : true, //终端窗口是否被关闭,默认为真
+		isShow : false //终端窗口是否被显示,默认为假
 	};
 
+	//控制台的相关配置文件
 	var terminalConfig = {
-		TIPS : '$_> '
+		TIPS : '$_> ', //控制台输入提示符(用来作为用户输入命令的分割符)
+		EDITORID : 'cmd', //控制台编辑器的ID
+		EOI : '\r\n' //换行符
 	};
+
+
+	//拓展string对象
+	String.prototype.trim=function(){
+　　    return this.replace(/(^\s*)|(\s*$)/g, "");
+　　};
+
+　　String.prototype.ltrim=function(){
+　　    return this.replace(/(^\s*)/g,"");
+　　};
+
+　　String.prototype.rtrim=function(){
+　　    return this.replace(/(\s*$)/g,"");
+　　};
 
 	//终端命令列表
 	var terminalCommands = {
 
 		currentFunctionName : '', //当前的函数名
 
-		clear : function (obj) { //清空控制台窗口内容
+		terminalEditorObj : '',
+
+		getTerminalAllContent : function(obj) { //获得控制台窗口的全部内容
+			var _this = $(obj);
+	    	var e = _this.val();
+	    	return e;
+		},
+
+		//为控制台窗口添加新行
+		appendNewLineInTerminal : function(e,obj,displayTips) {
+			displayTips = displayTips == null ? true : false;
+			if(displayTips) {
+				var TIPS = terminalConfig.TIPS;
+			}else{
+				var TIPS = '';
+			}
+			$(obj).val(e + '\r\n'+TIPS);
+		},
+
+		cmd : function (func,obj,args) { //根据函数名执行函数
+			if(func.length!==1){
+				var objCalled = eval('terminalCommands.'+func);
+
+				//清空命令行中的空白字符
+				for (var i = 0; i < args.length; i++) {
+					args[i] = args[i].trim();
+					args[i] = args[i].replace('\r\n','');
+				};
+
+				func = func.replace('\n','');//textarea中\n为换行符,替换之.否则会多出一行
+				var append = '\r\n       command "' + func +'" is not found';
+
+				if (typeof objCalled == 'undefined') {
+					terminalCommands.appendNewLineInTerminal(terminalCommands.getTerminalAllContent(obj)+append, obj);
+					return false;
+				}
+
+				new objCalled(obj,args);
+			}
+		},
+
+		clear : function (obj,args) { //清空控制台窗口内容
 			obj.val(terminalConfig.TIPS);
 		},
 
-		cmd : function (func,args) {
-			if(func.length!==1){
-
-				var obj = eval('terminalCommands.'+func);
-				new obj(args);
-			}
+		help : function (obj,args) { //输出帮助信息
+			var all = terminalCommands.getTerminalAllContent(obj);
+			all = all +'\r\n       this the help file';
+			console.log(args);
+			terminalCommands.appendNewLineInTerminal(all,obj);
 		}
 
 	};
@@ -61,7 +118,9 @@ $(function() {
 
 	//禁用右键菜单	
 	$('body').bind('contextmenu',function() {
-		return false;
+		if(!config.DEBUG){
+			return false;			
+		}
 	});
 
 	$('body').bind('selectstart',function() {
@@ -112,11 +171,11 @@ $(function() {
 	//关闭或打开终端窗口
 	var toggleTerminal = function() {
 		$('.mongo-terminal').fadeToggle(200);
-		$('#cmd').focus();
+		$("#"+terminalConfig.EDITORID).focus();
 		terminalStatus.isClosed = !terminalStatus.isClosed;
 		terminalStatus.isShow = !terminalStatus.isShow;
 		if(!terminalStatus.isMinified) {
-			$('#cmd').val("Welcome to MongoBro (version 1.0.0-preview20151010)\r\n\r\nRun 'h' to dplay the help index.\r\nRun 'h <command>' to display help for specific commands.\r\n\r\n"+terminalConfig.TIPS);
+			$("#"+terminalConfig.EDITORID).val("Welcome to MongoBro (version 1.0.0-preview20151010)\r\n\r\nRun 'h' to dplay the help index.\r\nRun 'h <command>' to display help for specific commands.\r\n\r\n"+terminalConfig.TIPS);
 		}
 		terminalStatus.isMinified = !terminalStatus.isMinified;
 		// terminalStatus.isMaxified = !terminalStatus.isMaxified;
@@ -184,22 +243,10 @@ $(function() {
 		toggleTerminal();
 	});
 
-	//获得控制台窗口的全部内容
-	var getTerminalAllContent = function(obj) {
-		var _this = $(obj);
-    	var e = _this.val();
-    	return e;
-	};
-
-	//为控制台窗口添加新行
-	var appendNewLineInTerminal = function(e,obj) {
-		$(obj).val(e + '\r\n'+terminalConfig.TIPS);
-	}
-
 	//获得用户输入的命令
 	var getCMD = function(obj,count) {
 		count = count == null ? 2 : count;
-		var e = getTerminalAllContent(obj);
+		var e = terminalCommands.getTerminalAllContent(obj);
     	var cmd = e.split(terminalConfig.TIPS);
     	var n = cmd.length-count;
     	e = '';
@@ -207,12 +254,12 @@ $(function() {
 	}
 
 	//控制台textarea回车捕获事件
-	$("#cmd").keydown(function(event) {
+	$("#"+terminalConfig.EDITORID).keydown(function(event) {
         if (event.keyCode === 13) {
-        	var all = getTerminalAllContent(this);
-        	appendNewLineInTerminal(all,this);
-        	var scrollTop = $("#cmd")[0].scrollHeight;  
-            $("#cmd").scrollTop(scrollTop);  
+        	var all = terminalCommands.getTerminalAllContent(this);
+        	terminalCommands.appendNewLineInTerminal(all,this);
+        	var scrollTop = $("#"+terminalConfig.EDITORID)[0].scrollHeight;  
+            $("#"+terminalConfig.EDITORID).scrollTop(scrollTop);  
         	handleCommands(getCMD(this));
         	all = '';
         	return false;
@@ -220,7 +267,7 @@ $(function() {
 	});
 
 	//控制台textarea按下退格键时判断前4个字符是否为'$_> ',如果是则阻止删除
-	$('#cmd').keydown(function(event){
+	$("#"+terminalConfig.EDITORID).keydown(function(event){
 		if (event.keyCode === 8) {
 			var cmd = getCMD(this,1);
 			if(cmd === ''){
@@ -232,14 +279,19 @@ $(function() {
 	//控制台命令处理函数
 	var handleCommands = function(cmd) {
 		var func = eval(terminalCommands.cmd);
-		console.log(cmd);
-		if(cmd != 'clear') {
-			console.log('ddd');
-			console.log($('#cmd'));
-			new func (cmd,$('#cmd'));	
-		}else{
-			new func (cmd);
+
+		var cmdSplit = cmd.split(' ');
+		var funcName = cmdSplit[0];
+		var args = cmdSplit.shift();
+
+		if(funcName == 'h'){
+			funcName = 'help';//因为命令行依赖的函数不能出现一个字符的情况,所以当输入为h时自动转换为help
 		}
+
+		//第二个参数是被调用函数的第一个参数
+		//所有被调用函数的第一个参数都为控制台编辑器的对象
+		//第二个参数为命令后面的空格数据(数组形式)
+		new func (funcName,$("#"+terminalConfig.EDITORID),cmdSplit);
 		
 	};
 
